8594d1a6a5049c14448c8a53dbd04ea63713f289
Michael <chiangshtaa@gmail.com>
2019-01-03 17:12:40 -0800

    titleToNumber

---
+++ leetcode/171-titleToNumber.js
@@ -0,0 +1,9 @@
+var titleToNumber = function(s) {
+  let result = 0;
+  for(let i = 0 ; i < s.length; i++) {
+    result = result * 26 + (s.charCodeAt(i) - 64);
+  }
+  return result;
+};
+
+console.log(titleToNumber('AAA'));
