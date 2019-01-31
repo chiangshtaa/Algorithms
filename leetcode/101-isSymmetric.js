e18a437485d7337dc99918496bd8b325aac8baa3
Michael <chiangshtaa@gmail.com>
2019-01-30 16:58:32 -0800

    add iterative solution to isSymmetric

--- leetcode/101-isSymmetric.js
+++ leetcode/101-isSymmetric.js
@@ -28,3 +28,28 @@ var isSymmetric = function(root) {
   return recurse(root.left, root.right);
   
 };
+
+// iterative solution
+// var isSymmetric = function(root) {
+//   let queue = [];
+//   queue.push(root, root);
+//   while (queue.length) {
+//     let t1 = queue.shift();
+//     let t2 = queue.shift();
+//     if (!t1 && !t2) {
+//       continue;
+//     } 
+//     if (!t1 || !t2) {
+//       return false;
+//     }
+//     if (t1.val !== t2.val) {
+//       return false;
+//     }
+//     queue.push(t1.left);
+//     queue.push(t2.right);
+//     queue.push(t1.right);
+//     queue.push(t2.left);
+    
+//   }
+//   return true;
+// }
