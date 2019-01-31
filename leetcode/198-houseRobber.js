1ea0fb5de894978d7f3b4d5fe90410870e1ed98b
Michael <chiangshtaa@gmail.com>
2019-01-30 18:32:58 -0800

    second time solution

--- leetcode/198-houseRobber.js
+++ leetcode/198-houseRobber.js
@@ -11,3 +11,18 @@ var rob = function(nums) {
   }
   return dp[dp.length - 1];
 };
+
+
+// Second time around
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  let dp = [0, 0];
+  for (let i = 2; i < nums.length + 2; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 2]);
+    
+  }
+  return dp[dp.length - 1];
+};
